6b74345a4c042132e75c612873ae7ce420e62318
ganlan <ganlan@zhuangdianwenhua.com>
2020-06-07 19:30:05 +0800

    fix:invokeMap,add check

--- src/invokeMap.ts
+++ src/invokeMap.ts
@@ -1,9 +1,10 @@
 import invoke from './invoke'
+import isArrayLike from './isArrayLike'
 
 function invokeMap(collection, path, args?) {
   let index = -1
   const isFunc = typeof path === 'function'
-  let result = collection != null ? new Array(collection.length) : []
+  let result = isArrayLike(collection) ? new Array(collection.length) : []
   while (++index < collection.length) {
     const value = collection[index]
     result[index] = isFunc ? path.apply(value, args) : invoke(value, path, args)
